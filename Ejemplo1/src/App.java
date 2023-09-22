8ba78f63a55de1c8e25d1790cce688e322b67e48
frodriguez0023 <frodriguez0023@CEP-AULA7-PC18.andalucia.fusp.ceu>
2023-09-22 16:02:31 +0200

    .

--- Ejemplo1/src/App.java
+++ Ejemplo1/src/App.java
@@ -2,15 +2,27 @@ import java.sql.SQLException;
 import java.util.List;
 
 import modelo.Actor;
+import servicios.ActoresServiceException;
 import servicios.ActoresServices;
 
 public class App {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		ActoresServices service = new ActoresServices();
-		List<Actor> actores = service.consultarActores();
-		for (Actor actor : actores) {
-			System.out.println(actor);
+		List<Actor> actores;
+
+		try {
+			try {
+				actores = service.consultarActores();
+				for (Actor actor : actores) {
+					System.out.println(actores);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		} catch (ActoresServiceException e) {
+			e.printStackTrace();
 		}
 
 	}
---
+++ Ejemplo1/src/servicios/ActoresServiceException.java
@@ -0,0 +1,37 @@
+package servicios;
+
+public class ActoresServiceException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5400147284858716679L;
+
+	public ActoresServiceException() {
+		super();
+		
+	}
+
+	public ActoresServiceException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		
+	}
+
+	public ActoresServiceException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+	public ActoresServiceException(String message) {
+		super(message);
+		
+	}
+
+	public ActoresServiceException(Throwable cause) {
+		super(cause);
+		
+	}
+
+	
+}
