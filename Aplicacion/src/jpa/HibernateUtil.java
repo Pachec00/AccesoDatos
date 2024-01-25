12c9be0a20d0a347533c408a7d324f73d0444a02
Fabio Rodríguez <65049200+Pachec00@users.noreply.github.com>
2024-01-25 14:33:04 +0100

    Add files via upload

---
+++ Aplicacion/src/jpa/HibernateUtil.java
@@ -0,0 +1,33 @@
+package jpa;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+import modelo.Usuario;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactoy;
+
+	public static SessionFactory getSessionFactoy() {
+		if (sessionFactoy == null) {
+			init();
+		}
+		return sessionFactoy;
+	}
+
+	private static void init() {
+		try {
+			ServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+			Metadata metadata = new MetadataSources(registry)
+					.addAnnotatedClass(Usuario.class)
+					.getMetadataBuilder().build();
+			sessionFactoy = metadata.getSessionFactoryBuilder().build();
+		} catch (Exception e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+}
